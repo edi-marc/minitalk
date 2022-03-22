1f7431f021a710be1dfec45e73a32ed32b7f24b4
edi-marc <edi-marc@student.42roma.it>
2022-03-22 15:16:04 +0100

    starting handling signals

--- server.c
+++ server.c
@@ -6,20 +6,37 @@
 /*   By: edi-marc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 19:51:03 by edi-marc          #+#    #+#             */
-/*   Updated: 2022/03/22 14:43:54 by edi-marc         ###   ########.fr       */
+/*   Updated: 2022/03/22 15:14:51 by edi-marc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static void	handler_sig(int num)
+{
+	if (num == SIGUSR1)
+		write(STDOUT_FILENO, "OK\n", 3);
+	else if (num == SIGUSR2)
+		write(STDOUT_FILENO, "OK\n", 3);
+}
+
 int	main(void)
 {
 	char	*pid;
+	struct sigaction	sa;
 
 	pid = ft_itoa(getpid());
 	if (!pid)
 		exit(EXIT_FAILURE);
 	ft_putendl_fd(pid, STDOUT_FILENO);
 	free(pid);
+	sa.sa_handler = handler_sig;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while(1)
+	{}
 	exit(EXIT_SUCCESS);
 }
